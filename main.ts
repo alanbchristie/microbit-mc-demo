c24f7d08db4198164a876a2a5309d556a1be09ed
Alan B. Christie <alan@kaitak.co.uk>
2020-12-04 08:13:13 +0000

    Adds comments & icon & threshold change

--- main.ts
+++ main.ts
@@ -3,41 +3,47 @@
  * 
  * ...for AA Alkaline batteries
  * 
- * A main-loop that monitors the analogue value on PIN-0 every few seconds. When it looks like a voltage is applied (i.e. anything above 0.5v) it displays either a "tick" or "cross" depending on the condition.
+ * A main-loop that monitors the analogue value on PIN-0 every few seconds. When it looks like a voltage is applied (i.e. anything above 0.5V) it displays either a "smile" or "frown" depending on the condition.
  * 
- * A voltage greater than 1.4v (voltage-threshold-good') is used to indicate a "healthy" battery with a "tick".
+ * A voltage greater than 1.35V (voltage-threshold-good') is used to indicate a "healthy" battery.
  * 
  * The indication is "held" for a few seconds when the battery is removed.
  * 
- * A "pull-down" resistor between PIN-0 and GND is required for this to work otherwise PIN-0 floats, typically to 3v. The example uses a 1M-ohm resistor - large enough to pull PIN-0 to ground but large enough not draw any significant current from the attached battery.
+ * A "pull-down" resistor between PIN-0 and GND is required for this to work otherwise PIN-0 floats, typically to 3V. The example uses a 1M-ohm resistor - large enough to pull PIN-0 to ground but large enough not draw any significant current from the attached battery.
  */
+// Reads the analogue value from PIN-0 and returns the "calibrated" voltage (a float)
 function getvoltage () {
     reading = pins.analogReadPin(AnalogPin.P0)
     voltagenow = reading / pin0at1v
     return voltagenow
 }
+// Given a calibrated voltage this function returns the new peak value.
 function setpeak (num: number) {
     if (num > voltagepeak) {
         voltagepeak = voltagenow
     }
     return voltagepeak
 }
+// Given a "calibrated" voltage this method returns true if the voltage is sufficient to consider that a battery is actually attached.
 function isconnected (num: number) {
     if (num >= connectedvoltage) {
         return true
     }
     return false
 }
+// Displays a "sad face" - usually called when a battery of poor condition is attached.
 function displaybad () {
-    led.setBrightness(128)
-    basic.showIcon(IconNames.No)
+    led.setBrightness(64)
+    basic.showIcon(IconNames.Sad)
 }
+// Displays a "smiley face" - usually called when a battery of good condition is attached.
 function displaygood () {
-    led.setBrightness(128)
-    basic.showIcon(IconNames.Yes)
+    led.setBrightness(64)
+    basic.showIcon(IconNames.Happy)
 }
+// Displays the application icon (low luminosity)
 function displayicon () {
-    led.setBrightness(32)
+    led.setBrightness(16)
     basic.showLeds(`
         . . # . .
         . # # # .
@@ -46,6 +52,7 @@ function displayicon () {
         . # # # .
         `)
 }
+// Initialises control variables and displays the application icon (a +/- symbol)
 let voltagepeak = 0
 let voltagenow = 0
 let reading = 0
@@ -54,7 +61,7 @@ let connectedvoltage = 0
 let idlecount = 0
 let idleclearthreshold = 1
 connectedvoltage = 0.5
-let voltagethresholdgood = 1.4
+let voltagethresholdgood = 1.35
 pin0at1v = 311.5
 displayicon()
 basic.forever(function () {
